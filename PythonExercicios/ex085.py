6a300261b876eb12c53238f3010029f8866336b8
JULIO CESAR DA SILVA <86634555+JulliusCaesar@users.noreply.github.com>
2021-07-07 21:59:52 -0300

    Exercicio 85 resolvido!

--- PythonExercicios/ex085.py
+++ PythonExercicios/ex085.py
@@ -1,3 +1,14 @@
 '''Crie um programa onde o usuário possa digitar sete valores numéricos e cadastre-os em uma lista única que mantenha
 separados os valores pares e impares. No final, mostre os valores pares e impares em ordem crescente.'''
-
+lista = [[], []]
+for c in range(0, 7):
+    num = (int(input(f'Digite o {c + 1}º valor: ')))
+    if num % 2 == 0:
+        lista[0].append(num)
+    else:
+        lista[1].append(num)
+print('-=' * 30)
+lista[0].sort()
+print(f'Os valores pares digitados foram:{lista[0]}')
+lista[1].sort()
+print(f'Os valores impares digitados foram: {lista[1]}')
