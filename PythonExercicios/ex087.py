f23c817f75eb1e8a1953c94e9dedbb4f97a3fdb8
JULIO CESAR DA SILVA <86634555+JulliusCaesar@users.noreply.github.com>
2021-07-08 20:46:05 -0300

    exercicio 87

--- PythonExercicios/ex087.py
+++ PythonExercicios/ex087.py
@@ -1,2 +1,23 @@
 '''Aprimore o desafio anterior, mostrando no final: a) A soma dos valores pares digitados. b) A soma dos valores
 da terceira coluna. c) O maior valor da segunda linha'''
+matriz = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+soma = somacoluna = maior = 0
+for l in range(0, 3):
+    for c in range(0, 3):
+        matriz[l][c] = int(input(f'Digite um valor para [{l}, {c}]:  '))
+        if matriz[l][c] % 2 == 0:
+            soma += matriz[l][c]
+        if c == 2:
+            somacoluna += matriz[l][c]
+        if l == 1:
+            if matriz[l][c] > maior:
+                maior = matriz[l][c]
+print('-=' * 30)
+for l in range(0, 3):
+    for c in range(0, 3):
+        print(f'[{matriz[l][c]:^5}]', end='')
+    print()
+print('-=' * 30)
+print(f'A soma dos valores pares é {soma}')
+print(f'A soma dos valores da terceira coluna é {somacoluna}')
+print(f'O maior valor da segunda linha é {maior}')
