27b9773e71e1348819aac47b8f0ef3e2ae47fcac
JULIO CESAR DA SILVA <86634555+JulliusCaesar@users.noreply.github.com>
2021-07-08 20:03:17 -0300

    Exercicio 86 resolvido!

--- PythonExercicios/ex086.py
+++ PythonExercicios/ex086.py
@@ -1,2 +1,13 @@
 '''Crie um programa que crie uma matriz de dimensão 3x3 e preencha com os valore lidos pelo teclado. No final,
 mostre a matriz na tela, com a formatação correta.'''
+matriz = [[], [], []]
+c = c2 = c3 = 0
+for c in range(0, 9):
+    matriz[c2].append(int(input(f'Digite um valor para [{c2}][{c3}]: ')))
+    c3 += 1
+    if c3 == 3:
+        c2 += 1
+        c3 = 0
+
+for p in matriz:
+    print(f'[  {p[0]} ] [  {p[1]} ] [  {p[2]} ]')
