d4eabc78eb6b37a1b29bf5c8ea2de9c761bb97f5
JULIO CESAR DA SILVA <86634555+JulliusCaesar@users.noreply.github.com>
2021-07-09 12:04:09 -0300

    exercicio 89 Junto com guanabara

--- PythonExercicios/ex089.py
+++ PythonExercicios/ex089.py
@@ -1,3 +1,27 @@
 '''Crie um prorama que leia o nome e duas notas de vários alunes e guarde tudo em uma lista composta. No final,
 mostre um boletim contendo a média de cada um e permita ue o usuário possa mostras as notas de cada aluno
 individualmente.'''
+ficha = list()
+while True:
+    nome = str(input('Nome: '))
+    nota1 = float(input('Nota1: '))
+    nota2 = float(input('Nota2: '))
+    media = (nota1 + nota2)/2
+    ficha.append([nome, [nota1, nota2], media])
+    resp = str(input('Quer continur? [s/n]: '))
+    if resp in 'Nn':
+        break
+print('-=' * 30)
+print(f'{"No.":<4}{"NOME":<10}{"MÉDIA":>8}')
+print('-' * 26)
+for i, a in enumerate(ficha):
+    print(f'{i:<4}{a[0]:<10}{a[2]:>8.1f}')
+while True:
+    print('-' * 35)
+    opc = int(input('Mostrar nota de qual aluno? (999 interrompe) '))
+    if opc == 999:
+        print('FINALIZANDO...')
+        break
+    if opc <= len(ficha) - 1:
+        print(f'Notas de {ficha[opc][0]} são {ficha[opc][1]}')
+print('<<< VOLTE SEMPRE >>>')
