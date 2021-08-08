0bfe2f7ab73dafb561e71a46a3546aaff20e8d43
JULIO CESAR DA SILVA <86634555+JulliusCaesar@users.noreply.github.com>
2021-08-08 19:40:06 -0300

    Corrigido com Guanabara

--- PythonExercicios/ex093.py
+++ PythonExercicios/ex093.py
@@ -3,14 +3,12 @@ e quantas partidas ele jogou. Depois vai ler a quantidade de gols feitos em cada
 guardado em um dicionário, incluindo o total de gols feitos durante o campeonato'''
 gols = list()
 identificaJogador = dict()
-somaGols = 0
 identificaJogador['nome'] = str(input('Nome do Jogador: '))
 numPartidas = int(input(f'Quantas partidas {identificaJogador["nome"]} jogou? '))
 for contador in range(0, numPartidas):
     gols.append(int(input(f'Quantos gols na partida {contador}? ')))
-    somaGols += gols[contador]
-identificaJogador['gols'] = gols
-identificaJogador['total'] = somaGols
+identificaJogador['gols'] = gols[:]
+identificaJogador['total'] = sum(gols)
 print('-=' * 50)
 print(identificaJogador)
 print('-=' * 50)
