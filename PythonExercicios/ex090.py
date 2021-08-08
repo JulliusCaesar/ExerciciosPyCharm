e17b3d96b508bf0f18a7f1b084d2ed5d828f1037
JULIO CESAR DA SILVA <86634555+JulliusCaesar@users.noreply.github.com>
2021-08-08 18:20:05 -0300

    pequena correção

--- PythonExercicios/ex090.py
+++ PythonExercicios/ex090.py
@@ -1,9 +1,9 @@
 '''Faça um programa que leia o nome e média de um aluno, guardando também a situação em um dicionário.
 No final, mostre o conteúdo da etrutura na tela.'''
-aluno = {'Nome': '', 'Média': 0, 'Situação': ''}
-
+aluno = dict()
 aluno['Nome'] = str(input('Nome: '))
 aluno['Média'] = float(input(f'Média de {aluno["Nome"]}: '))
+print('-=' * 40)
 if aluno['Média'] >= 7:
     aluno['Situação'] = 'Aprovado'
 elif aluno['Média'] <= 5:
