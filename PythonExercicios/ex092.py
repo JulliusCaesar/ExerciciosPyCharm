e03e6301b1a2f818a698b05cfab6ebba709c3771
JULIO CESAR DA SILVA <86634555+JulliusCaesar@users.noreply.github.com>
2021-08-08 19:23:07 -0300

    Corrigido com Guanabara

--- PythonExercicios/ex092.py
+++ PythonExercicios/ex092.py
@@ -1,21 +1,20 @@
 '''Crie um programa que leia nome, ano de nascimento e carteira de trabalho e cadastre os (com idade) em um dicionário
 se por acaso a CTPS for diferente de ZERO, o dicionário receberá também o ano de contratação e o salário. Calcule
 e acrescente além da idade, com quantos anos a pessoas vai se aposentar.'''
-from datetime import date
+from datetime import datetime
 carteiraTrabalho = dict()
 
 while True:
     carteiraTrabalho['nome'] = str(input("Nome: "))
-    anoAtual = date.today().year
     anoNascimento = int(input('Ano de Nascimento: '))
-    carteiraTrabalho['idade'] = anoAtual - anoNascimento
+    carteiraTrabalho['idade'] = datetime.now().year - anoNascimento
     carteiraTrabalho['ctps'] = int(input('Carteira de Trabalho (0 não tem): '))
     if carteiraTrabalho['ctps'] == 0:
         break
     carteiraTrabalho['contratação'] = int(input('Ano de Contratação: '))
     idadeContratacao = carteiraTrabalho['contratação'] - anoNascimento
-    carteiraTrabalho['aposentadoria'] = idadeContratacao + 35
     carteiraTrabalho['salário'] = float(input('Salário: '))
+    carteiraTrabalho['aposentadoria'] = idadeContratacao + 35
     break
 print('-=' * 40)
 for Key, Value in carteiraTrabalho.items():
