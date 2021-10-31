29f7dde765753781f48064e264c211ef0b362283
JULIO CESAR DA SILVA <86634555+JulliusCaesar@users.noreply.github.com>
2021-10-31 18:07:35 -0300

    Correção de exercicios

--- PythonExercicios/ex105.py
+++ PythonExercicios/ex105.py
@@ -2,3 +2,30 @@
 com as seguintes informações: -Quantidade de notas, -A maior nota, -A menor nota, A média da turma,
 -A situação(opcional). Adicione também as DocStrings da função"""
 
+
+def notas(*n, sit=False):
+    """
+    -> Função para analisar notas e situações de vários alunos.
+    :param n: Uma ou mais notas dos alunos (aceitas várias)
+    :param sit: valor opcional, indicando se deve ou não adicionar a situação.
+    :return: dicionário com várias informações sobre a situação da turma.
+    """
+    r = dict()
+    r['Total'] = len(n)
+    r['Maior'] = max(n)
+    r['Menor'] = min(n)
+    r['Média'] = sum(n)/len(n)
+    if sit:
+        if r['Média'] >= 7:
+            r['Situação'] = 'BOA'
+        elif r['Média'] >= 5:
+            r['Situação'] = 'RAZOÁVEL'
+        else:
+            r['Situação'] = 'RUIM'
+    return r
+
+
+# Programa Principal
+
+resp = notas(5.5, 2.5, 9, 8.5,sit=True)
+print(resp)
