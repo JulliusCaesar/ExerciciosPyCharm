f89a6f7c3baa1d8c01fe30e169cdff7ae6e5dd4a
JULIO CESAR DA SILVA <86634555+JulliusCaesar@users.noreply.github.com>
2021-11-01 11:44:57 -0300

    Novos exercicios

---
+++ PythonExercicios/ex107/moeda.py
@@ -0,0 +1,18 @@
+def metade(valor):
+    return valor / 2
+
+
+def dobro(valor):
+    return valor * 2
+
+
+def aumentar(valor):
+     aumento = valor * 0.10
+     valor += aumento
+     return valor
+
+
+def diminuir(valor):
+    diminui = valor * 0.13
+    valor -= diminui
+    return valor
