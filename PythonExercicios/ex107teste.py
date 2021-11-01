7c3f5481c4eb07db1b3c5dcac00df95e6bb5d5f6
JULIO CESAR DA SILVA <86634555+JulliusCaesar@users.noreply.github.com>
2021-11-01 16:04:18 -0300

    Melhorando exercicios

--- PythonExercicios/ex107teste.py
+++
@@ -1,8 +0,0 @@
-"""Crie um módulo chamado moeda.py que tenha as funções incorporadas aumentar(), diminuir(), dobro() e medate().
-Faça também um programa que importe esse módulo e use algumas dessas funções."""
-from ex107 import moeda
-p = float(input('Digite o preço: R$ '))
-print(f'A metade de {p} é {moeda.metade(p)}')
-print(f'O dobro de {p} é {moeda.dobro(p)}')
-print(f'Aumentando 10%, temos {moeda.aumentar(p)}')
-print(f'Reduzindo 13%, temos {moeda.diminuir(p)}')
